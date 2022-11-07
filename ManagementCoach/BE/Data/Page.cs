10a942d36fde30fa5c08117f9dbb8e341ff2c37b
comaybay <comaybayy@gmail.com>
2022-11-07 21:49:01 +0700

    Add data classes

---
+++ ManagementCoach/BE/Data/Page.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementCoach.BE
+{
+	public class Page<T>
+	{
+		/// <summary>
+		/// Danh sách các kết quả của trang hiện tại
+		/// </summary>
+		public List<T> Items { get; set; }
+
+		/// <summary>
+		/// Trang hiện tại
+		/// </summary>
+		public int CurrentPage { get; set; }
+
+		/// <summary>
+		/// Tổng số lượng trang chứa kết quả
+		/// </summary>
+		public int PageCount { get; set; }
+	}
+}
