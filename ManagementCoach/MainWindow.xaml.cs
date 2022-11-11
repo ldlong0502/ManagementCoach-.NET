b78bb1564f0464098d9cd60907d5988adbdc9a98
comaybay <comaybayy@gmail.com>
2022-11-11 14:57:58 +0700

    fix app not shutdown when close window

--- ManagementCoach/MainWindow.xaml.cs
+++ ManagementCoach/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ManagementCoach
         {
             InitializeComponent();
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+			Application.Current.Shutdown();
+		}
     }
 }
