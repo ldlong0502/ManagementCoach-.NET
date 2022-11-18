7f0a38690e9dd9729db4efdcf03c1460133f79f6
comaybay <comaybayy@gmail.com>
2022-11-18 11:25:04 +0700

    add RepoTrip

---
+++ ManagementCoach/BE/Data/Input/InputTrip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ManagementCoach.BE.Data.Input
+{
+	public class InputTrip
+	{
+		public int RouteId { get; set; }
+		public int CoachId { get; set; }
+		public int DriverId { get; set; }
+		public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+		public bool Cancelled { get; set; }
+	}
+}
---
+++ ManagementCoach/BE/Repositories/RepoTrip.cs
@@ -0,0 +1,56 @@
+﻿using ManagementCoach.BE.Data.Input;
+using ManagementCoach.BE.Data;
+using ManagementCoach.BE.Entities;
+using ManagementCoach.BE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementCoach.BE.Repositories
+{
+	public class RepoTrip : Repository
+	{
+		public bool TripExists(int id) => Context.Trips.Any(d => d.Id == id);
+
+		public Result<ModelTrip> InsertTrip(InputTrip input)
+		{
+			var trip = Map.To<Trip>(input);
+			Context.Trips.Add(trip);
+			Context.SaveChanges();
+			return new Result<ModelTrip>() { Success = true, Payload = Map.To<ModelTrip>(trip) };
+		}
+
+		public ModelTrip GetTrip(int id)
+		{
+			return Map.To<ModelTrip>(Context.Trips.Where(c => c.Id == id).FirstOrDefault());
+		}
+
+		public Result<ModelTrip> UpdateTrip(int id, InputTrip input)
+		{
+			if (!TripExists(id))
+				return new Result<ModelTrip> { Success = false, ErrorMessage = "Trip with this Id do not exist" };
+
+			var trip = Context.Trips.Where(c => c.Id == id).FirstOrDefault();
+
+			trip = Map.To(input, trip);
+			Context.SaveChanges();
+
+			return new Result<ModelTrip> { Success = true, Payload = Map.To<ModelTrip>(trip) };
+		}
+
+		public Result DeleteTrip(int id)
+		{
+			if (!TripExists(id))
+				return new Result { Success = false, ErrorMessage = "Trip with this Id do not exist" };
+
+			var trip = new Trip() { Id = id };
+			Context.Trips.Attach(trip);
+			Context.Trips.Remove(trip);
+			Context.SaveChanges();
+
+			return new Result { Success = true };
+		}
+	}
+}
