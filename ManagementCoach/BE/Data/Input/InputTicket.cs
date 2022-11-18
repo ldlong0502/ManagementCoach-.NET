c86efe59fc74d0815e450158aa750299d2d1cf05
comaybay <comaybayy@gmail.com>
2022-11-18 11:24:52 +0700

    add RepoTicket

---
+++ ManagementCoach/BE/Data/Input/InputTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ManagementCoach.BE.Data.Input
+{
+	public class InputTicket
+	{
+		public int TripId { get; set; }
+		public int CustomerId { get; set; }
+	}
+}
