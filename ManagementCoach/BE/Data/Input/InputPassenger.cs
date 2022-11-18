e960c0143cb4dcb8417fd64b467a26bcf31a7f04
comaybay <comaybayy@gmail.com>
2022-11-18 09:52:42 +0700

    add RepoPassenger

---
+++ ManagementCoach/BE/Data/Input/InputPassenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ManagementCoach.BE.Data.Input
+{
+	public class InputPassenger
+	{
+		public string Name { get; set; }
+		public string IdCard { get; set; }
+		public string Gender { get; set; }
+		public DateTime Dob { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+		public bool Blocked { get; set; }
+		public string Notes { get; set; }
+	}
+}
--- ManagementCoach/BE/Repositories/RepoDriver.cs
+++ ManagementCoach/BE/Repositories/RepoDriver.cs
@@ -12,15 +12,21 @@ namespace ManagementCoach.BE.Repositories
 {
 	public class RepoDriver : Repository
 	{
-		public bool IdCardExists(string idCard)
-		{
-			return Context.Drivers.Any(d => d.IdCard == idCard);
-		}
+		public bool IdCardExists(string idCard) => Context.Drivers.Any(d => d.IdCard == idCard);
+		public bool EmailExists(string email) => Context.Drivers.Any(d => d.Email == email);
+		public bool PhoneExists(string phone) => Context.Drivers.Any(d => d.Phone == phone);
+		public bool DriverExists(int id) => Context.Drivers.Any(d => d.Id == id);
 
 		public Result<ModelDriver> InsertDriver(InputDriver input)
 		{
 			if (IdCardExists(input.IdCard))
 				return new Result<ModelDriver>() { Success = false, ErrorMessage = "Driver with this Id card already exist." };
+
+			if (EmailExists(input.Email))
+				return new Result<ModelDriver> { Success = false, ErrorMessage = "Driver with this email already exist." };
+
+			if (PhoneExists(input.Phone))
+				return new Result<ModelDriver> { Success = false, ErrorMessage = "Driver with this phone already exist." };
 
 			var driver = Map.To<Driver>(input);
 			Context.Drivers.Add(driver);
@@ -38,7 +44,13 @@ namespace ManagementCoach.BE.Repositories
 			var driver = Context.Drivers.Where(c => c.Id == coachId).FirstOrDefault();
 
 			if (driver.IdCard != input.IdCard && IdCardExists(input.IdCard))
-				return new Result<ModelDriver> { Success = false, ErrorMessage = "Coach with this registration already exist." };
+				return new Result<ModelDriver> { Success = false, ErrorMessage = "Driver with this registration already exist." };
+
+			if (driver.Email != input.Email && EmailExists(input.Email))
+				return new Result<ModelDriver> { Success = false, ErrorMessage = "Driver with this email already exist." };
+
+			if (driver.Phone != input.Phone && PhoneExists(input.Phone))
+				return new Result<ModelDriver> { Success = false, ErrorMessage = "Driver with this phone already exist." };
 
 			driver = Map.To(input, driver);
 			Context.SaveChanges();
@@ -49,7 +61,7 @@ namespace ManagementCoach.BE.Repositories
 		public Result DeleteDriver(int id)
 		{
 			if (!DriverExists(id))
-				return new Result { Success = false, ErrorMessage = "Coach with this Id do not exist" };
+				return new Result { Success = false, ErrorMessage = "Driver with this Id do not exist" };
 
 			var driver = new Driver() { Id = id };
 			Context.Drivers.Attach(driver);
@@ -58,7 +70,5 @@ namespace ManagementCoach.BE.Repositories
 
 			return new Result { Success = true };
 		}
-
-		public bool DriverExists(int id) => Context.Drivers.Any(d => d.Id == id);
 	}
 }
