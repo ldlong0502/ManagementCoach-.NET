6d3b66586d814d39189174a502430627bc2ea3db
comaybay <comaybayy@gmail.com>
2022-11-18 11:29:50 +0700

    add more functionality for RepoCoachSeat

--- ManagementCoach/BE/Repositories/RepoCoachSeat.cs
+++ ManagementCoach/BE/Repositories/RepoCoachSeat.cs
@@ -15,13 +15,13 @@ namespace ManagementCoach.BE.Repositories
         public bool CoachSeatExists(int id)
             => Context.CoachSeats.Any(c => c.Id == id);
 
-        public bool CoachSeatNameExists(string name)
+        public bool NameExists(string name)
             => Context.CoachSeats.Any(c => c.Name == name);
 
         public Result<ModelCoachSeat> InsertCoachSeat(InputCoachSeat input)
         {
             throw new NotImplementedException();
-            if (CoachSeatNameExists(input.Name))
+            if (NameExists(input.Name))
                 return new Result<ModelCoachSeat> { Success = false, ErrorMessage = "Coach seat with this name already exist." };
 
             var coachSeat = Map.To<CoachSeat>(input);
@@ -30,7 +30,12 @@ namespace ManagementCoach.BE.Repositories
             return new Result<ModelCoachSeat> { Success = true, Payload = Map.To<ModelCoachSeat>(coachSeat) };
         }
 
-        public List<ModelCoachSeat> GetCoachSeats(int coachId)
+		public ModelCoachSeat GetCoachSeat(int seatId)
+		{
+			return Map.To<ModelCoachSeat>(Context.CoachSeats.Where(c => c.Id == seatId).FirstOrDefault());
+		}
+
+		public List<ModelCoachSeat> GetCoachSeats(int coachId)
         {
             return Context.CoachSeats
 						  .Where(c => c.CoachId == coachId)
@@ -39,5 +44,33 @@ namespace ManagementCoach.BE.Repositories
 						  .ToList();
         }
 
-    }
+		public Result<ModelCoachSeat> UpdateCoachSeat(int id, string seatName)
+		{
+			if (!CoachSeatExists(id))
+				return new Result<ModelCoachSeat> { Success = false, ErrorMessage = "CoachSeat with this Id do not exist" };
+
+			var coachSeat = Context.CoachSeats.Where(c => c.Id == id).FirstOrDefault();
+
+			if (coachSeat.Name != seatName && NameExists(seatName))
+				return new Result<ModelCoachSeat> { Success = false, ErrorMessage = "CoachSeat with this name already exist." };
+
+			coachSeat.Name = seatName;
+			Context.SaveChanges();
+
+			return new Result<ModelCoachSeat> { Success = true, Payload = Map.To<ModelCoachSeat>(coachSeat) };
+		}
+
+		public Result DeleteCoachSeat(int id)
+		{
+			if (!CoachSeatExists(id))
+				return new Result { Success = false, ErrorMessage = "CoachSeat with this Id do not exist" };
+
+			var coachSeat = new CoachSeat() { Id = id };
+			Context.CoachSeats.Attach(coachSeat);
+			Context.CoachSeats.Remove(coachSeat);
+			Context.SaveChanges();
+
+			return new Result { Success = true };
+		}
+	}
 }
