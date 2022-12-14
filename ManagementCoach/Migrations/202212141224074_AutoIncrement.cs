b0531b91041311c25de850c32b682ac8c23ee90f
comaybay <comaybayy@gmail.com>
2022-12-14 19:30:22 +0700

    fix auto increment trips

---
+++ ManagementCoach/Migrations/202212141224074_AutoIncrement.cs
@@ -0,0 +1,18 @@
+﻿namespace ManagementCoach.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AutoIncrement : DbMigration
+    {
+        public override void Up()
+        {
+			Sql("CREATE SEQUENCE dbo.\"Trips_Id_seq\" INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 OWNED BY dbo.\"Trips\".\"Id\"");
+			Sql("ALTER TABLE dbo.\"Trips\" ALTER COLUMN \"Id\" SET DEFAULT nextval('dbo.\"Trips_Id_seq\"'::regclass)");
+		}
+
+		public override void Down()
+        {
+        }
+    }
+}
